ea8629fe766ae168cb4c8358b2c4b06af677632c
Anton <anton.homm@gmail.com>
2023-02-15 14:12:38 +0100

    Basic Frame versions 17/17

---
+++ src/main/java/com/example/gitarrappapi_17/GitarrAppApi17Application.java
@@ -0,0 +1,13 @@
+package com.example.gitarrappapi_17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitarrAppApi17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitarrAppApi17Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitarrappapi_17/GitarrAppApi17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitarrappapi_17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitarrAppApi17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
